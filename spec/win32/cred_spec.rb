939b75dd524cbb8cf70b0fe53ee62419eb03930e
Ben Vidulich <ben@vidulich.co.nz>
2016-02-24 00:18:51 +1300

    Remove the fake spec example

--- spec/win32/cred_spec.rb
+++ spec/win32/cred_spec.rb
@@ -4,8 +4,4 @@ describe Win32::Cred do
   it 'has a version number' do
     expect(Win32::Cred::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
